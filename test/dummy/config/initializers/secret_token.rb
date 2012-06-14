7694a6850ffc3a59a19f7c665ab006cf8d26ea39
Red Squadron <wedge@redsquadron.com>
2012-06-14 13:04:10 -0700

    first commit - enginex skeleton

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'ffea90670d8b08245299751c6fd0d9eee698de36c50eb8864b22c00aad4b06b888886e182b0d98b46721919670dad5b52a28e540f7b82f1d45eab2434b259328'
